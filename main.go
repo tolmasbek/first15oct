013109d17d19beef5727e81ca30c6866634e192f
tolmasbek <farmonovtt@gmail.com>
2020-10-15 19:08:56 +0400

    fourth commit

--- main.go
+++ main.go
@@ -8,5 +8,8 @@ func main() {
 	fmt.Println("ftt Git Hub!!")
 	fmt.Println("Farmonov Tolmasbek")
 
-	fmt.Println("fmt.Println("Farmonov Tolmasbek")	
+	fmt.Println("Farmonov Tolmasbek")	
+
+	fmt.Println("Tolmasbek")
+
 }
